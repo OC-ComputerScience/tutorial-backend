a13e4159c231384d775c5e87d861750c402a2555
dbnorth <david.north@oc.edu>
2025-10-03 12:10:05 -0500

    update for cors

--- server.js
+++ server.js
@@ -9,53 +9,21 @@ db.sequelize.sync();
 
 const app = express();
 
+// Also use the cors middleware as backup
 var corsOptions = {
-  origin: function (origin, callback) {
-    // Allow requests with no origin (like mobile apps or curl requests)
-    if (!origin) return callback(null, true);
-    
-    console.log('CORS check for origin:', origin);
-    
-    if (origin === 'http://localhost:8081') {
-      callback(null, true);
-    } else {
-      console.log('CORS blocked origin:', origin);
-      callback(new Error('Not allowed by CORS'));
-    }
-  },
-  credentials: true,
-  methods: ['GET', 'POST', 'PUT', 'DELETE', 'OPTIONS'],
-  allowedHeaders: ['Content-Type', 'Authorization', 'X-Requested-With', 'Accept'],
-  preflightContinue: false,
-  optionsSuccessStatus: 200
-};
-// Add request logging middleware
-app.use((req, res, next) => {
-  console.log(`${req.method} ${req.url} - Origin: ${req.headers.origin}`);
-  console.log('Request headers:', req.headers);
-  next();
-});
-
+  origin: "http://localhost:8081",
+  credentials: true
+}
 app.use(cors(corsOptions));
-app.options('*', cors(corsOptions));
 
-// Add response logging middleware
-app.use((req, res, next) => {
-  const oldSend = res.send;
-  res.send = function(data) {
-    console.log('Response headers:', res.getHeaders());
-    oldSend.apply(this, arguments);
-  };
-  next();
-});
 
 // parse requests of content-type - application/json
 app.use(express.json());
 // parse requests of content-type - application/x-www-form-urlencoded
 app.use(express.urlencoded({ extended: true }));
   
-
-app.use("/tutorial", routes); // Load the routes from the routes folder
+// Load the routes from the routes folder
+app.use("/tutorial", routes); 
 
 
 // set port, listen for requests
